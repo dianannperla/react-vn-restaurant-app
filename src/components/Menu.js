d7f3a1cac33062202ef91d5f9da99d42726ee913
dianannperla <waafirmelyza@hotmail.com>
2025-09-05 19:23:53 +0100

    feat: initial commit for src/components/Menu.js

---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div>
+      <h2 className="text-xl font-semibold">Thực đơn</h2>
+      {/* Thực đơn sẽ được hiển thị tại đây */}
+    </div>
+  );
+};
+
+export default Menu;
