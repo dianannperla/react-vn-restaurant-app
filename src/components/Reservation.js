03b9da8707423102c2691e67cc3560ff4572b037
dianannperla <waafirmelyza@hotmail.com>
2025-09-05 19:23:55 +0100

    feat: initial commit for src/components/Reservation.js

---
+++ src/components/Reservation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Reservation = () => {
+  return (
+    <div>
+      <h2 className="text-xl font-semibold">Đặt bàn</h2>
+      {/* Form đặt bàn sẽ được hiển thị tại đây */}
+    </div>
+  );
+};
+
+export default Reservation;
